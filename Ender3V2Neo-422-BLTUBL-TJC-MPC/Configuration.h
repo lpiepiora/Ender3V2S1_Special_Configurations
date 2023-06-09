8d3d66265d6bd8e89c2abedc42c268d22fba0f9d
Lukasz Piepiora <lpiepiora@gmail.com>
2023-06-09 21:41:06 +0200

    Update offsets for Ender 3 v2 Neo - Cooling Fan Duct 2x 4020 Fans

--- Ender3V2Neo-422-BLTUBL-TJC-MPC/Configuration.h
+++ Ender3V2Neo-422-BLTUBL-TJC-MPC/Configuration.h
@@ -1542,7 +1542,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -39, -12, 0 }  // CR Touch Default Mount
+#define NOZZLE_TO_PROBE_OFFSET { -47, -10, 0 }  // CR Touch Default Mount
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
